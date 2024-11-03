a980c2ee9106bbc319002a12bd643cc5dbd81e56
UpendraJaiswal <37846481+Upendra-Jaiswal@users.noreply.github.com>
2024-11-03 18:27:49 +0530

    Added support for multiple comma-separated numbers

--- stringCalculator.test.js
+++ stringCalculator.test.js
@@ -1,15 +1,17 @@
+const add = require("./stringCalculator");
 
-const add = require('./stringCalculator');
-
-test('returns 0 for an empty string', () => {
+test("returns 0 for an empty string", () => {
   expect(add("")).toBe(0);
 });
 
-test('returns the number itself for a single number input', () => {
-    expect(add("1")).toBe(1);
-  });
-  
-  test('returns the sum of two comma-separated numbers', () => {
-    expect(add("1,2")).toBe(3);
-  });
-  
+test("returns the number itself for a single number input", () => {
+  expect(add("1")).toBe(1);
+});
+
+test("returns the sum of two comma-separated numbers", () => {
+  expect(add("1,2")).toBe(3);
+});
+
+test("returns the sum of multiple comma-separated numbers", () => {
+  expect(add("1,2,3,4")).toBe(10);
+});
