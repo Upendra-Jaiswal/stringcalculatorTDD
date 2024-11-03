6a8619d302b1a5a5e25c6d46b75c8a5dcfc25920
UpendraJaiswal <37846481+Upendra-Jaiswal@users.noreply.github.com>
2024-11-03 18:32:32 +0530

    Added support for custom delimiters

--- stringCalculator.js
+++ stringCalculator.js
@@ -1,7 +1,17 @@
 function add(numbers) {
-  if (numbers === "") return 0;
-  const numArray = numbers.split(/,|\n/); // Regex to split by comma or newline
-  return numArray.reduce((sum, num) => sum + parseInt(num), 0);
-}
+    if (numbers === "") {
+      return 0; // Handle empty string
+    }
+    
+    if (numbers.startsWith("//")) {
+      const delimiter = numbers[2];
+      numbers = numbers.substring(4);
+      return numbers.split(delimiter).reduce((sum, num) => sum + parseInt(num), 0);
+    }
+    
+    const numArray = numbers.split(/,|\n/);
+    return numArray.reduce((sum, num) => sum + parseInt(num), 0);
+  }
+  
 
 module.exports = add;
